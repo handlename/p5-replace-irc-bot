800ecfd8fa04f80eb43105a5ff4037ca3a2f2bd8
handlename <nagata@handlena.me>
2013-07-07 15:56:08 +0900

    first commit

---
+++ bot.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use AnySan;
+use AnySan::Provider::IRC;
+use Log::Minimal;
+
+use App::Options(
+    option => {
+        host    => { type => 'string', required => 1 },
+        port    => { type => 'string', default  => '6666' },
+        ssl     => { type => 'bool',   default  => 0 },
+        nick    => { type => 'string', required => 1 },
+        channel => { type => 'string', required => 1 },
+    },
+);
+my %opts = %App::options;
+
+my $password = password();
+
+my $messages = {};
+
+my $irc = irc(
+    $opts{host},
+    port       => $opts{port},
+    enable_ssl => $opts{ssl},
+    password   => $password,
+    key        => $opts{host},
+    nickname   => $opts{nick},
+    channels   => { $opts{channel} => {} },
+);
+
+AnySan->register_listener(
+    replace => {
+        cb => sub {
+            my ($receive) = @_;
+
+            if ($receive->message =~ qr!s/([^/]+)/([^/]*)/! &&
+                defined $messages->{$receive->from_nickname})
+            {
+                my $before = $1;
+                my $after  = $2;
+
+                my $message = $messages->{$receive->from_nickname};
+                $message =~ s/$before/$after/;
+                $receive->send_replay($message);
+
+                infof '%s: %s, %s -> %s',
+                    $receive->from_nickname,
+                    $receive->message,
+                    $messages->{$receive->from_nickname},
+                    $message;
+            }
+            else {
+                $messages->{$receive->from_nickname} = $receive->message;
+            }
+        },
+    },
+);
+
+AnySan->run();
+
+sub password {
+    print 'password []:';
+
+    system 'stty -echo';
+    my $password = <STDIN>;
+    system 'stty echo';
+    print "\n";
+
+    chomp $password;
+
+    return $password;
+}
